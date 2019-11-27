074e1257ec56d6719c896bef812dcbee83cd7892
Nahuel Veron <nahuelav96@gmail.com>
2019-11-27 19:07:29 -0300

    Wording

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,7 @@ const About = () => <Card>
   I'm a software development with 5 years of experience on building reliable and production-ready products.
   As big fan of Javascript, I haven't moved into any other languages, but took positions doing several things, from being Full-Stack developer to Mobile (with React Native) or Desktop (with Electron).
   <br />
-  I'm always eager for improvement and testing new technologies. I'd love to keep care of the code quality and architectural improvements, I'd like to work on small to medium teams on Startups.
+  I'm always eager for improvement and testing new technologies. I'd love to keep care of the code quality and architectural improvements, I enjoy working on small to medium teams on startup companies.
 </Card>
 
 export default About
