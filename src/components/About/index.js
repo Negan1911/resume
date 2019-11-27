ed7409bf8dcf97cbcbbde5a988b7a9a3388859e6
Nahuel Veron <nahuelav96@gmail.com>
2019-11-27 18:25:45 -0300

    Updating about, removing github workflow

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,9 +3,10 @@ import { Title, Card } from '../UI'
 
 const About = () => <Card>
   <Title>Nice to meet you</Title>
-  I had worked as Frontend Engineer for nearly 4 years, combined with prior backend experience in another languages. Almost one and half year ago i took a position as Javascript Leader, where i started to teach my coworkers to implement React instead of Angular, and develop open source skeletons to making easy to work with a new stack of frontend technologies.
-  While i was responsible of review the frontend code committed to the projects, i was also reviewing and developing Node.js projects, mostly involved with MongoDB and/or AWS Lambda.
-  My aim as Lead Architect is to be constantly learning new technologies and good practices that makes us maintain high quality products and help the others to understand them, since two of my strong points are that i learn relatively fast and can predict when we're doing the things in the bad way (and research how to do in the proper way).
+  I'm a software development with 5 years of experience on building reliable and production-ready products.
+  As big fan of Javascript, I haven't moved into any other languages, but took positions doing several things, from being Full-Stack developer to Mobile (with React Native) or Desktop (with Electron).
+  <br />
+  I'm always eager for improvement and testing new technologies. I'd love to keep care of the code quality and architectural improvements, I'd like to work on small to medium teams on Startups.
 </Card>
 
 export default About
